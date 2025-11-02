729b8c47ffeeabc53babcc92b1f01cf661665812
KunJeong <kunjeong99@naver.com>
2025-11-02 16:21:48 +0900

    fix: missing conflict files

--- p4spec/lib/runtime-dynamic-il/rel.ml
+++ p4spec/lib/runtime-dynamic-il/rel.ml
@@ -3,7 +3,9 @@ module InputHint = Runtime_static.Rel.InputHint
 
 (* Relation *)
 
-type t = Extern of InputHint.t | Defined of InputHint.t * rulegroup list
+type t =
+  | Extern of InputHint.t
+  | Defined of nottyp * InputHint.t * rulegroup list
 
 let to_string = function
   | Extern _ -> "extern relation"
