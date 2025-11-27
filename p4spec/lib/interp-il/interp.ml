3aa32409b50bda577b92bf82176b92eb82fc7e9a
KunJeong <kunjeong99@naver.com>
2025-11-27 15:52:53 +0900

    feat(interp-il): Add table function support in Il interpreter

--- p4spec/lib/interp-il/interp.ml
+++ p4spec/lib/interp-il/interp.ml
@@ -1101,8 +1101,7 @@ module Make (Arch : Sim.ARCH) : Sim.INTERP_IL = struct
     match func with
     | Func.Extern -> invoke_extern_func ctx id targs values_input
     | Func.Builtin -> invoke_builtin_func ctx id targs values_input
-    | Func.Table (params, tblrows) ->
-        invoke_table_func ctx id params tblrows values_input
+    | Func.Table (_, tblrows) -> invoke_table_func ctx id tblrows values_input
     | Func.Defined (tparams, clauses) ->
         invoke_defined_func ctx id tparams clauses targs values_input
 
@@ -1141,11 +1140,64 @@ module Make (Arch : Sim.ARCH) : Sim.INTERP_IL = struct
       let* ctx, value_output = invoke_func_builtin' () in
       Ok (ctx, value_output)
 
-  and invoke_table_func (ctx : Ctx.t) (id : id) (params : param list)
-      (tblrows : tblrow list) (values_input : value list) :
-      (Ctx.t * value) attempt =
-    (ctx, id, params, tblrows, values_input) |> ignore;
-    failwith "TODO: invoke_table_func"
+  and match_table_row (ctx_caller : Ctx.t) (ctx_callee : Ctx.t)
+      (tblrow : tblrow) (values_input : value list) :
+      Ctx.t * arg list * prem list * exp =
+    let _, args_input, exp_output, prems = tblrow.it in
+    check
+      (List.length args_input = List.length values_input)
+      tblrow.at "arity mismatch while matching table row";
+    let ctx = assign_args ctx_caller ctx_callee args_input values_input in
+    (ctx, args_input, prems, exp_output)
+
+  and invoke_table_func (ctx : Ctx.t) (id : id) (tblrows : tblrow list)
+      (values_input : value list) : (Ctx.t * value) attempt =
+    let attempt_rows () =
+      let attempt_row' (ctx_local : Ctx.t) (prems : prem list)
+          (exp_output : exp) : (Ctx.t * value) attempt =
+        let* ctx_local = eval_prems ctx_local prems in
+        let ctx_local, value_output = eval_exp ctx_local exp_output in
+        let ctx_local = Ctx.trace_close ctx_local in
+        let ctx = Ctx.trace_commit ctx ctx_local.trace in
+        Ok (ctx, value_output)
+      in
+      let attempt_rows =
+        List.mapi
+          (fun idx_row tblrow ->
+            let attempt_row () : (Ctx.t * value) attempt =
+              (* Create a subtrace for the table row *)
+              let ctx_local = Ctx.localize ctx in
+              let ctx_local =
+                Ctx.trace_open_dec ctx_local id idx_row values_input
+              in
+              (* Try to match the table row *)
+              let ctx_local, args_input, prems, exp_output =
+                match_table_row ctx ctx_local tblrow values_input
+              in
+              (* Try evaluating the row *)
+              attempt_row' ctx_local prems exp_output
+              |> nest id.at
+                   (F.asprintf "application of table row %s%s failed" id.it
+                      (Il.Print.string_of_args args_input))
+            in
+            attempt_row)
+          tblrows
+      in
+      choice attempt_rows
+    in
+    if Cache.is_cached_func id.it then (
+      let cache_result = Cache.Cache.find !func_cache (id.it, values_input) in
+      match cache_result with
+      | Some (subtraces, value_output) ->
+          let ctx = Ctx.trace_replace ctx subtraces in
+          Ok (ctx, value_output)
+      | None ->
+          let* ctx, value_output = attempt_rows () in
+          let subtraces = Trace.wipe_subtraces ctx.trace in
+          Cache.Cache.add !func_cache (id.it, values_input)
+            (subtraces, value_output);
+          Ok (ctx, value_output))
+    else attempt_rows ()
 
   and invoke_defined_func (ctx : Ctx.t) (id : id) (tparams : tparam list)
       (clauses : clause list) (targs : targ list) (values_input : value list) :
