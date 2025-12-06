b80677ab4976b0edbdae7a78ef2d43cb19671009
jaehyun1ee <99jaehyunlee@kaist.ac.kr>
2025-12-06 18:54:55 +0900

    refactor: del unused error_deep in shallowbind.ml

--- p4spec/lib/elaborate/dataflow/shallowbind.ml
+++ p4spec/lib/elaborate/dataflow/shallowbind.ml
@@ -2,19 +2,12 @@ open Il.Ast
 open Error
 open Util.Source
 
-(* Collect binding identifiers,
-   while enforcing shallow binding *)
+(* Check that binding patterns are shallow
 
-let error_deep (exp : exp) =
-  error exp.at
-    (Format.asprintf "%s not allowed in shallow binding"
-       (Il.Print.string_of_exp exp))
-
-let error_deep_msg (exp : exp) (msg : string) =
-  error exp.at
-    (Format.asprintf "%s not allowed in shallow binding: %s"
-       (Il.Print.string_of_exp exp)
-       msg)
+   Shallow binding allows only:
+    - variables
+    - upcasts of variables or case expressions
+    - case expressions over variables *)
 
 (* Expressions *)
 
@@ -22,7 +15,7 @@ let check_shallow_exp (exp : exp) : bool =
   let rec is_iterated_var exp =
     match exp.it with
     | VarE _ -> true
-    | IterE (e, _) -> is_iterated_var e
+    | IterE (exp, _) -> is_iterated_var exp
     | _ -> false
   in
   match exp.it with
