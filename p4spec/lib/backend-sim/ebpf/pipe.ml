31e69d7cfa2d3a3e43c5ed263ea0d871788225b9
pacokwon <haechank@gmail.com>
2026-02-10 14:50:21 +0900

    fix: parameter placeholders for multicast interface in ebpf and placeholder

--- p4spec/lib/backend-sim/ebpf/pipe.ml
+++ p4spec/lib/backend-sim/ebpf/pipe.ml
@@ -286,15 +286,15 @@ struct
     error_no_region
       "add_mirror_session is not implemented for the ebpf simulator"
 
-  let mc_mgrp_create =
+  let mc_mgrp_create (_value_arch : Value.t) (_mgid : int) : Value.t =
     error_no_region
       "mc_mgrp_create is not implemented for the ebpf simulator"
 
-  let mc_node_create =
+  let mc_node_create (_value_arch : Value.t) (_rid : int) (_port : int) : Value.t =
     error_no_region
       "mc_node_create is not implemented for the ebpf simulator"
 
-  let mc_node_associate =
+  let mc_node_associate (_value_arch : Value.t) (_mgid : int) (_handle : int) : Value.t =
     error_no_region
       "mc_node_associate is not implemented for the ebpf simulator"
 
