07a1053453776d2a1988f5042a2fa185b22cff4e
Sehyuk Ahn <shyukahn@gmail.com>
2026-04-01 17:30:52 +0900

    fix: retain full info of parameters that are functions

--- p4spec/lib/interp/interp-sl/interp.ml
+++ p4spec/lib/interp/interp-sl/interp.ml
@@ -150,7 +150,8 @@ module Make (Arch : Sim.ARCH) : Sim.INTERP_SL = struct
       (value : value) : Ctx.t =
     match param.it with
     | ExpP (_typ, exp) -> assign_param_exp ctx_callee exp value
-    | DefP id -> assign_param_def ctx_caller ctx_callee id value
+    | DefP (id, _tparams, _params, _typ) ->
+        assign_param_def ctx_caller ctx_callee id value
 
   and assign_params (ctx_caller : Ctx.t) (ctx_callee : Ctx.t)
       (params : param list) (values : value list) : Ctx.t =
