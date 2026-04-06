9ae21061e31c0900eace4017a218c2395f8bb086
jaehyun1ee <99jaehyunlee@kaist.ac.kr>
2026-04-06 19:46:28 +0900

    feat: misc optimization, quite minimal effect tho

--- p4spec/lib/runtime/value/match.ml
+++ p4spec/lib/runtime/value/match.ml
@@ -8,7 +8,7 @@ open Util.Source
 
 (* Whether a value belongs to a type (including subtyping) *)
 
-let rec sub (find_typdef_opt : TId.t -> Type.Typdef.t option)
+let rec sub_ (find_typdef_opt : TId.t -> Type.Typdef.t option)
     (find_func : FId.t -> tparam list * typ list * typ) (typ : typ)
     (value : value) : bool =
   match typ.it with
@@ -26,51 +26,51 @@ let rec sub (find_typdef_opt : TId.t -> Type.Typdef.t option)
       | Param | Defining _ -> error typ.at "unexpected type variable"
       | Extern -> ( match value.it with ExternV _ -> true | _ -> false)
       | Defined (tparams, deftyp) -> (
-          let theta = TIdMap.of_lists tparams targs in
           match (deftyp.it, value.it) with
           | PlainT typ, _ ->
+              let theta = TIdMap.of_lists tparams targs in
               let typ = Type.Subst.subst_typ theta typ in
-              sub find_typdef_opt find_func typ value
+              sub_ find_typdef_opt find_func typ value
           | StructT typfields, StructV valuefields
             when List.length typfields = List.length valuefields ->
+              let theta = TIdMap.of_lists tparams targs in
               List.for_all2
                 (fun (atom_t, typ) (atom_v, value) ->
                   Atom.eq atom_t.it atom_v.it
                   &&
                   let typ = Type.Subst.subst_typ theta typ in
-                  sub find_typdef_opt find_func typ value)
+                  sub_ find_typdef_opt find_func typ value)
                 typfields valuefields
           | VariantT typcases, CaseV (mixop_v, values_inner) ->
+              let theta = TIdMap.of_lists tparams targs in
               List.exists
                 (fun typcase ->
                   let nottyp, _, _ = typcase in
                   let mixop_t, typs_inner = nottyp.it in
                   Mixop.eq mixop_t mixop_v
                   &&
-                  let typs_inner =
-                    List.map (Type.Subst.subst_typ theta) typs_inner
-                  in
-                  subs find_typdef_opt find_func typs_inner values_inner)
+                  let typs_inner = Type.Subst.subst_typs theta typs_inner in
+                  subs_ find_typdef_opt find_func typs_inner values_inner)
                 typcases
           | _ -> false))
   | TupleT typs -> (
       match value.it with
       | TupleV values ->
           List.length typs = List.length values
-          && List.for_all2 (sub find_typdef_opt find_func) typs values
+          && List.for_all2 (sub_ find_typdef_opt find_func) typs values
       | _ -> false)
   | IterT (typ_inner, Opt) -> (
       match value.it with
       | OptV value_opt -> (
           match value_opt with
           | Some value_inner ->
-              sub find_typdef_opt find_func typ_inner value_inner
+              sub_ find_typdef_opt find_func typ_inner value_inner
           | None -> true)
       | _ -> true)
   | IterT (typ_inner, List) -> (
       match value.it with
       | ListV values ->
-          List.for_all (sub find_typdef_opt find_func typ_inner) values
+          List.for_all (sub_ find_typdef_opt find_func typ_inner) values
       | _ -> false)
   | FuncT (tparams_t, typs_params_t, typ_ret_t) -> (
       match value.it with
@@ -80,8 +80,26 @@ let rec sub (find_typdef_opt : TId.t -> Type.Typdef.t option)
             typ_ret_t tparams_v typs_params_v typ_ret_v
       | _ -> false)
 
-and subs (find_typdef_opt : TId.t -> Type.Typdef.t option)
+and subs_ (find_typdef_opt : TId.t -> Type.Typdef.t option)
     (find_func : FId.t -> tparam list * typ list * typ) (typs : typ list)
     (values : value list) : bool =
   List.length typs = List.length values
-  && List.for_all2 (sub find_typdef_opt find_func) typs values
+  && List.for_all2 (sub_ find_typdef_opt find_func) typs values
+
+(* Entry point *)
+
+let cache find_typdef_opt =
+  let cache : (string, Type.Typdef.t option) Hashtbl.t = Hashtbl.create 8 in
+  fun tid ->
+    match Hashtbl.find_opt cache tid.it with
+    | Some td_opt -> td_opt
+    | None ->
+        let td_opt = find_typdef_opt tid in
+        Hashtbl.add cache tid.it td_opt;
+        td_opt
+
+let sub find_typdef_opt find_func typ value =
+  sub_ (cache find_typdef_opt) find_func typ value
+
+let subs find_typdef_opt find_func typs values =
+  subs_ (cache find_typdef_opt) find_func typs values
