243343970f4f3f3ccfb8121405a23810898dae64
pacokwon <haechank@gmail.com>
2026-09-04 14:45:40 +0900

    feat: dune rules for nano tests

--- p4spec/test/sim/nano_test.ml
+++ p4spec/test/sim/nano_test.ml
@@ -89,8 +89,8 @@ let sim_command =
      and mode =
        Command.Param.choose_one
          [
-           flag "il" no_arg ~doc:"Run IL interpreter"
-           |> map ~f:(fun b -> Core.Option.some_if b IL_mode);
+           flag "al" no_arg ~doc:"Run AL interpreter"
+           |> map ~f:(fun b -> Core.Option.some_if b AL_mode);
            flag "sl" no_arg ~doc:"Run SL interpreter"
            |> map ~f:(fun b -> Core.Option.some_if b SL_mode);
          ]
